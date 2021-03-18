ca300707c480e0146954bb934aac0aab9c1c78fd
JulienPrince007 <prince@sowellapp.com>
2021-03-18 10:30:12 +0300

    denied access

--- src/boot/navigation-guards.js
+++ src/boot/navigation-guards.js
@@ -1,11 +1,17 @@
 import { LocalStorage } from "quasar";
+import { Notify } from "quasar";
+
 export default async ({ store, router }) => {
   router.beforeEach((to, from, next) => {
     const token = LocalStorage.getItem("token");
     if (token === null && to.path !== "/login") {
       next("/login");
       if (from.path === "/login") {
-        alert("access denied");
+        Notify.create({
+          type: "negative",
+          position: "top",
+          message: "access denied",
+        });
       }
     } else {
       next();

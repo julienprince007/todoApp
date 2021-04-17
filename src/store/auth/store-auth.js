f35a061c05b4fd918d34ebaa7a1861fbae6acdb0
JulienPrince007 <prince@sowellapp.com>
2021-04-17 07:49:14 +0300

    last

--- src/store/auth/store-auth.js
+++ src/store/auth/store-auth.js
@@ -1,43 +1,10 @@
-import { LocalStorage } from "quasar";
+const state = {};
 
-const state = {
-  token: null,
-  loading: false,
-};
+const mutations = {};
 
-const mutations = {
-  SET_USER(state, user) {
-    state.user = user;
-  },
-  SET_TOKEN(state, token) {
-    state.token = token;
-    LocalStorage.set("token", state.token);
-    this.$router.push("/");
-  },
-  SET_CLEAR(state) {
-    state.token = null;
-    LocalStorage.clear();
-  },
-  SET_LOADING(state) {
-    state.loading = true;
-  },
-  SET_LOADING_END(state) {
-    state.loading = false;
-  },
-};
+const actions = {};
 
-const actions = {
-  logoutUser({ commit }) {
-    const that = this;
-    commit("SET_CLEAR");
-    that.$router.push("/login");
-  },
-};
-
-const getters = {
-  isSignedIn: (state) => state.token,
-  loading: (state) => state.loading,
-};
+const getters = {};
 
 export default {
   namespaced: true,
